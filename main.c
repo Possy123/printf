c65a936516b2ecc3996f1280a9943a44c6467a89
Emmanuel Ajibokun <emmanuelajibokun9@gmail.com>
2023-06-26 06:02:21 -0700

    updated main.c file to print the return value of _printf(), and decrease printf() counter variable by 1

--- main.c
+++ main.c
@@ -1,12 +1,14 @@
 #include "main.h"
+#include <stdio.h>
 
-int main (void)
+int main(void)
 {
 	char i = 'm';
 	char *str = "Today is a good day";
 
 	_printf("Hello, How are you doing? This is %% sign\n");
-	_printf( "I said %s, to you, %c\n", str, i);
+	_printf("I said %s, to you, %c\n", str, i);
+	printf("'Hello': %d\n", _printf("Hello\n"));
 
-	return 0;
+	return (0);
 }
